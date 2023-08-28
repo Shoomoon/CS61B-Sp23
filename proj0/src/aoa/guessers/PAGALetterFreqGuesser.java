b1a8e7f8ed1c40e6371e324213b2d8bc0d2d023d
Shoomoon <informationz@163.com>
2023-08-28 11:59:51 -0700

    Map used to finished PAGALetterFreqGuesser.java and passed all test cases.

--- proj0/src/aoa/guessers/PAGALetterFreqGuesser.java
+++ proj0/src/aoa/guessers/PAGALetterFreqGuesser.java
@@ -1,6 +1,7 @@
 package aoa.guessers;
 
 import aoa.utils.FileUtils;
+import edu.princeton.cs.algs4.In;
 
 import java.util.*;
 
@@ -17,20 +18,38 @@ public class PAGALetterFreqGuesser implements Guesser {
     public char getGuess(String pattern, List<Character> guesses) {
         // TODO: Fill in this method.
         List<String> wordsPool = new ArrayList<>();
+        Map<Character, List<Integer>> charsIndexPattern = getCharsIndexMap(pattern);
         Set<Character> noShowChars = new HashSet<>(guesses);
-        Set<Character> showAllChars = new HashSet<>();
+//        Set<Character> showAllChars = new HashSet<>();
         for (int i = 0; i < pattern.length(); i++) {
             noShowChars.remove(pattern.charAt(i));
-            showAllChars.add(pattern.charAt(i));
+//            showAllChars.add(pattern.charAt(i));
         }
         noShowChars.remove('-');
-        showAllChars.remove('-');
+//        showAllChars.remove('-');
         for (String word: words) {
-            if (match(pattern, word, noShowChars, showAllChars)) {
+            if (pattern.length() == word.length() && match(word, noShowChars, charsIndexPattern)) {
                 wordsPool.add(word);
             }
         }
+//        for (String word: words) {
+//            if (match(pattern, word, noShowChars, showAllChars)) {
+//                wordsPool.add(word);
+//            }
+//        }
         return getSimpleGuess(wordsPool, guesses);
+    }
+    private Map<Character, List<Integer>> getCharsIndexMap(String word) {
+        Map<Character, List<Integer>> charsIndex = new HashMap<>();
+        for (int i = 0; i < word.length(); i++) {
+            char cp = word.charAt(i);
+            if (!charsIndex.containsKey(cp)) {
+                charsIndex.put(cp, new ArrayList<>());
+            }
+            charsIndex.get(cp).add(i);
+        }
+        charsIndex.remove('-');
+        return charsIndex;
     }
     private char getSimpleGuess(List<String> wordsPool, List<Character> guesses) {
         Map<Character, Integer> charsFreq = getFrequencyMap(wordsPool);
@@ -54,23 +73,38 @@ public class PAGALetterFreqGuesser implements Guesser {
         }
         return mostCommonLetter;
     }
-    private boolean match(String pattern, String target, Set<Character> noShowChars, Set<Character> showAllChars) {
-        // any char c in showAllChars, then all char c in target must be shown
-        // any char c in noShowChars, must not be in target
-        if (pattern.length() != target.length()) {
-            return false;
+    private boolean match(String target, Set<Character> noShowChars, Map<Character, List<Integer
+            >>charsIndexPattern) {
+        Map<Character, List<Integer>> charsIndexTarget = getCharsIndexMap(target);
+        for (char ct: charsIndexTarget.keySet()) {
+            if (noShowChars.contains(ct)) {
+                return false;
+            }
         }
-        for (int i = 0; i < pattern.length(); i++) {
-            char cp = pattern.charAt(i);
-            char ct = target.charAt(i);
-            if (noShowChars.contains(ct) ||
-                    cp != '-' && cp != ct ||
-                    cp == '-' & showAllChars.contains(ct)) {
+        for (char cp: charsIndexPattern.keySet()) {
+            if (!charsIndexTarget.containsKey(cp) || !charsIndexPattern.get(cp).equals(charsIndexTarget.get(cp))) {
                 return false;
             }
         }
         return true;
     }
+//    private boolean match(String pattern, String target, Set<Character> noShowChars, Set<Character> showAllChars) {
+//        // any char c in showAllChars, then all char c in target must be shown
+//        // any char c in noShowChars, must not be in target
+//        if (pattern.length() != target.length()) {
+//            return false;
+//        }
+//        for (int i = 0; i < pattern.length(); i++) {
+//            char cp = pattern.charAt(i);
+//            char ct = target.charAt(i);
+//            if (noShowChars.contains(ct) ||
+//                    cp != '-' && cp != ct ||
+//                    cp == '-' & showAllChars.contains(ct)) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
     public Map<Character, Integer> getFrequencyMap(List<String> wordsPool) {
         // TODO: Fill in this method.
         Map<Character, Integer> res = new HashMap<>();
