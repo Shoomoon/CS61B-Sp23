d84beb0a330323cb5a763830fa4e0e3d853ed504
Shoomoon <informationz@163.com>
2023-08-30 17:38:55 -0700

    Finished lab2 and passed all test cases.

--- lab02/src/adventure/BeeCountingStage.java
+++ lab02/src/adventure/BeeCountingStage.java
@@ -3,6 +3,7 @@ package adventure;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdRandom;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -16,6 +17,7 @@ public class BeeCountingStage implements AdventureStage {
     public BeeCountingStage(In in) {
         this.in = in;
         this.responses = Map.of("go", new SpeciesListStage(in));
+        this.input = new ArrayList<>();
     }
 
     /**
@@ -83,8 +85,8 @@ public class BeeCountingStage implements AdventureStage {
      */
     private int sumInput() {
         int sum = 0;
-        for (int i = 0; i <= this.input.size(); i++) {
-            sum += Integer.parseInt(this.input.get(i));
+        for (String s : this.input) {
+            sum += Integer.parseInt(s);
         }
         return sum;
     }
--- lab02/src/adventure/MachineStage.java
+++ lab02/src/adventure/MachineStage.java
@@ -78,26 +78,11 @@ public class MachineStage implements AdventureStage {
     }
 
     public static int mysteryMax(int a, int b) {
-        int w = (b - a) >> 31;
-        int z = ~(b - a) >> 31;
-
-        int max = b & w | a & z;
-        return max;
+        return Math.max(a, b);
     }
 
     public static int mysteryAdd(int a, int b) {
-        int x = a, y = b;
-        int xor, and, temp;
-        and = x & y;
-        xor = x ^ y;
-
-        while (and != 0) {
-            and <<= 1;
-            temp = xor ^ and;
-            and &= xor;
-            xor = temp;
-        }
-        return xor;
+        return a + b;
     }
 
     /**
@@ -123,10 +108,13 @@ public class MachineStage implements AdventureStage {
      * Returns the sum of all elements in x.
      */
     public static int arraySum(int[] x) {
+        if (x == null) {
+            return 0;
+        }
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + mysteryAdd(sum, x[i]);
+            sum = mysteryAdd(sum, x[i]);
             i = i + 1;
         }
         return sum;
